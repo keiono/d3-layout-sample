323c6a99f43e6d7bc6186aa9c470d879714c4260
keiono <keiono@gmail.com>
2013-12-09 01:36:56 -0800

    Grunt file has been updated to copy data directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -257,6 +257,7 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
                         'styles/fonts/{,*/}*.*',
+                        'data/*.*',
                         'bower_components/sass-bootstrap/fonts/*.*'
                     ]
                 }]
